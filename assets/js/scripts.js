ea3ba6abfdc92f0d855af50fe9a7129ad40ed8ec
Couragyn <couragyn@highwaythree.com>
2018-03-28 10:26:23 -0700

    fixed jquery issue

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,30 @@
 $(document).ready(function() {
-    $('#mainNav li a').on('click', function(event) {
-      console.log($(this).parent().find('a'));
-      $(this).parent().find('a').removeClass('active');
-      $(this).addClass('active');
+
+  // Add/remove active class for clicking on a menu item
+  $('#mainNav li a').on('click', function(event) {
+    $('#mainNav li a').removeClass('active');
+    $(this).addClass('active');
   });
 
-    $(window).on('scroll', function() {
-    //     $('.menuItem').each(function() {
-    //         if($(window).scrollTop() >= $(this).offset().top) {
-    //             var id = $(this).attr('id');
-    //             $('#nav nav a').removeClass('active');
-    //             $('#nav nav a[href=#'+ id +']').addClass('active');
-    //         }
-    //     });
+  // Add/remove active class for scrolling by menu
+  $(window).on('scroll', function() {
+    console.log('scroll');
+    $('.menuItem').each(function() {
+        if($(window).scrollTop() >= $(this).offset().top) {
+          var id = $(this).attr('id');
+          if (id === undefined) {
+            $('#mainNav li a').removeClass('active');
+          } else {
+            $('#mainNav li a').removeClass('active');
+            $('#mainNav li a[href*=' + id + ']').addClass('active');
+          }
+        }
     });
+  });
 
-   });
+  // moves the window down 1 pixel to fix skills issue
+  window.addEventListener("hashchange", function () {
+      window.scrollTo(window.scrollX, window.scrollY + 1);
+  });
+
+});
